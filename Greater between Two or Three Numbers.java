a0b9c8689f185f7f179abe26bbdf9165e88d058f
Aman Jain <83697226+amanjain5132@users.noreply.github.com>
2022-09-16 12:30:35 +0530

    Create Greater between Two or Three Numbers.java

---
+++ Greater between Two or Three Numbers.java
@@ -0,0 +1,16 @@
+package SmartProgramming;
+
+public class GreaterNumber {
+    public static void main(String[] args) {
+        int a = 10, b = 20, c = 229;
+        if (a > b && a > c){
+            System.out.println("A is Grater");
+        }else if (b > a && b > c){
+            System.out.println("B is grater");
+        }
+        else
+        {
+            System.out.println("C is Grater");
+        }
+    }
+}
